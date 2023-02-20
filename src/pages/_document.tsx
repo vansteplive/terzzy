02727b50681c8d804d132a7e7ea2871c7ff2cf71
Ivan Stepanov <van.step2186@gmail.com>
2023-02-20 10:04:45 +0300

    Исправление по шрифтам

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.cdnfonts.com/css/clash-display"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
